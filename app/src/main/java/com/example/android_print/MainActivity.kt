41920eefbfccb678e94cd97b14908d573bd407e9
hens <121116111@qq.com>
2024-08-05 18:36:47 +0800

    chore: init

---
+++ app/src/main/java/com/example/android_print/MainActivity.kt
@@ -0,0 +1,57 @@
+package com.example.android_print
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.ClickableText
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.android_print.ui.theme.AndroidprintTheme
+import com.hens.lib.print.Print
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AndroidprintTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding).clickable {
+
+                            // 在这里处理点击事件
+                            println("=========Text clicked!")
+
+                            Print().sayHello("fffff")
+                        }
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier,
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    AndroidprintTheme {
+        Greeting("Android")
+    }
+}
---
+++ print/src/main/java/com/hens/lib/print/Print.kt
@@ -0,0 +1,7 @@
+package com.hens.lib.print
+
+public class  Print {
+    fun sayHello(name: String): String {
+        return "Hello, $name!"
+    }
+}
